727fd9505bf77df8bde65dcc322b8dcd9c968f97
Adams Mohammed <moadams847@gmail.com>
2021-08-27 12:14:05 +0000

    make year in footer dynamic

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// nav links border buttom on hover
+// nav links border buttom on hover=====================
 const navLink = $(".nav-link");
 
 navLink.on("mouseenter", function () {
@@ -9,10 +9,8 @@ navLink.on("mouseleave", function () {
   $(this).removeClass("border-bottom border-altprimary border-1");
 });
 
-// dynamic date=====
+// dynamic date========================================
 const now = new Date();
 const currentYear = now.getFullYear();
-// console.log(`get full year: ${currentYear}`);
-
-const span = document.querySelector(".dynamicDate");
-span.textContent = currentYear;
+const span = $(".dynamicDate");
+span.text(currentYear);
